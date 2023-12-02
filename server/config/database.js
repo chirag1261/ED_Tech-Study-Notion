3a3f385f8967dc895081cc819e77d3127357f5a1
Chirag Kumar <chiragkrkashyap@gmail.com>
2023-12-02 23:10:02 +0530

    First Commit StudyNotion

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected Successfully!"))
+    .catch((e) => {
+      console.log("MongoDB Connection Failed!");
+      console.log(e);
+      process.exit(1);
+    });
+};
